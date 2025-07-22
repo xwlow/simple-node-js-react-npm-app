6947e7b8452d5419218cfae1078cbcf340877bd9
xwlow <2301949@sit.singaporetech.edu.sg>
2025-07-22 15:48:30 +0800

    additional testing for lab

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,23 +11,28 @@
 // ]);
 
 
-import js from "@eslint/js";
 import globals from "globals";
+import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-
+import pluginSecurity from "eslint-plugin-security";
+import securityNode from "eslint-plugin-security-node";
+import noUnsanitized from "eslint-plugin-no-unsanitized";
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  js.configs.recommended,
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    }
-  },
-  {
-    files: ["**/*.{js,jsx}"],
-    ...pluginReact.configs.flat.recommended,
-  }
+{files: ["**/*.{js,mjs,cjs,jsx}"]},
+{languageOptions: { globals: globals.browser }},
+pluginJs.configs.recommended,
+pluginReact.configs.flat.recommended,
+{
+plugins: {
+security: pluginSecurity,
+securityNode: securityNode,
+noUnsanitized: noUnsanitized,
+},
+rules: {
+...pluginJs.configs.recommended.rules,
+...pluginReact.configs.flat.recommended.rules,
+"security/detect-eval-with-expression": "error",
+}
+}
 ];
